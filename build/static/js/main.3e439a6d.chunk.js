395fc1645e1de7f2a5fb8a14a6e85f68d9ff9034
Will Kang <willkang@umich.edu>
2021-07-05 15:40:10 -0400

    React adjustment

---
+++ build/static/js/main.3e439a6d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpthetatau-thetagamma"]=this["webpackJsonpthetatau-thetagamma"]||[]).push([[0],{9:function(t,e,a){"use strict";a.r(e);var n=a(1),c=a.n(n),r=a(3),h=a.n(r),s=a(0);var i=function(){return Object(s.jsx)("div",{children:"Theta Tau - Theta Gamma Website"})};h.a.render(Object(s.jsx)(c.a.StrictMode,{children:Object(s.jsx)(i,{})}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.3e439a6d.chunk.js.map
